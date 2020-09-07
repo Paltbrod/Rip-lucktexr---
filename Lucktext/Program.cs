f6069a0815b581b855f32923d4e891ddece99524
Lukas <lukas.tuna03@gmail.com>
2020-09-07 09:32:52 +0200

    Initial commit

---
+++ Lucktext/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Lucktext
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("What is your name");
+            string name = Console.ReadLine();
+
+
+
+
+            if (name == "pog")
+            {
+                Console.WriteLine("hello mr pog. Poggers to finally meet you (*o*)");
+
+            }
+            else if (name == "pogger")
+            {
+                Console.WriteLine("It cant be... are you really the legendray pogbaby from the old legends?");
+            }
+
+            else
+            {
+                Console.WriteLine("Kinda cringe am i right lads *the lads agree*");
+            }
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
